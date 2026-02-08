bd1db7bff8fcfdec283d79f51a1b12b6f2b1502b
ECO-HZ <zhubiaoooo@outlook.com>
2026-02-08 19:40:14 +0800

    Update Figure S3.R

--- Code folder/Figure S3.R
+++ Code folder/Figure S3.R
@@ -1,95 +1,194 @@
 ################################################################################
-################################## Figure S4 ###################################
+################################# Figure S3 ####################################
 ################################################################################
-
-# Loading R packages
+library(ggplot2) # version 3.5.2
 library(openxlsx) # version 4.2.5.2
-library(vegan) # version 2.6-4
+library(emmeans) # version 1.10.6
 
-############################## (Field survey part) #############################
-# Soil sample grouping information
-Field_group <- read.xlsx("Field_data_group.xlsx", sheet = "Field_group", rowNames = T, colNames = T)
-Field_group$Sample_ID <- rownames(Field_group)
+# Custom style
+mytheme = theme(
+  legend.position = "none",
+  panel.grid=element_blank(), 
+  strip.background = element_rect(color="black", fill="white", size=0.5, linetype="solid"),
+  strip.text.x = element_text(size = 9, color = "black"), # face = "bold.italic"
+  legend.title = element_blank(),
+  legend.key = element_blank(),
+  legend.text = element_text(size = 10),
+  legend.background = element_rect(fill = NA), #axis.ticks.length = unit(0.4,"lines"), 
+  axis.ticks = element_line(color='black'),
+  axis.line = element_line(colour = "black"), 
+  axis.title.x = element_text(colour='black', size=14),
+  axis.title.y = element_text(colour='black', size=14),
+  axis.text = element_text(colour='black',size=12),
+  plot.title = element_textbox(
+    size = 14, color = "black", fill = "grey90",
+    box.color = "grey50",padding = margin(5, 5, 5, 5), margin = margin(b = 0),       
+    halign = 0.5, width = grid::unit(1, "npc")), #r = unit(3, "pt")     
+  plot.tag = element_text(size = 16, face = "bold")) 
 
-# Soil sample abundance information
-Field_otu_raw <- read.xlsx("Field_data_raw_ASVs.xlsx", sheet = "raw_otu", colNames = T, rowNames = T)
-Field_otu_raw <- Field_otu_raw[ ,Field_group$Sample_ID]
+# Loading the grouping metadata of soil samples
+Field_group = read.xlsx("Field_data_group.xlsx", sheet = "field_group", rowNames = T, colNames = T)
+Field_group$Sample_ID = rownames(Field_group)
 
-par(las = 1, mar = c(5, 5, 4, 2))
-Field_min_depth <- min(rowSums(t(Field_otu_raw)))
-rarecurve(as.data.frame(t(Field_otu_raw)), step = 100, 
-          col = "black", cex = 0.6, label = FALSE, xlab = "", ylab = "", main = "")
-abline(v = Field_min_depth, col = "black", lty = 2, lwd = 2)
-title(main = "Field survey", xlab = "Number of sequences", ylab = "Number of ASVs")
+# Data Transformation
+Field_group$SRL <- log10(Field_group$SRL)
+Field_group$Wcont <- sqrt(Field_group$Wcont)
+Field_group$Soil_N <- sqrt(Field_group$Soil_N)
+Field_group$Year <- as.factor(Field_group$Year)
+Field_group$Site <- factor(Field_group$Site, levels = c("Guangzhou","Guilin","Changsha","Wuhan","Zhengzhou","Tai'an"))
+Field_group$Origin <- factor(Field_group$Origin, levels = c("Native","Exotic"))
+
+# Create an offset mapping
+offset_mapping <- c("2018" = -0.2, "2020" = 0, "2021" = 0.2)
+
+## Soil total nitrogen content
+data_Soil_N <- Field_group[,c("Origin", "Year" ,"Site", "Latitude", "Soil_N")]
+
+mod4 = lm(Soil_N ~ Year * Latitude, data = data_Soil_N)
+anova(mod4)
+shapiro.test(residuals(mod4))
 
 
-###########################  (Greenhouse exp. part) ############################
-Green_group <- read.xlsx("Greenhouse_data_group.xlsx", sheet = "green_group", colNames = T, rowNames = T)
-Green_group$Sample_ID <- rownames(Green_group)
+mod4_emtrends <- emtrends(mod4, pairwise ~ Year, var = "Latitude")
+test(mod4_emtrends, adjust = "BH")
 
-Green_otu_raw <- read.xlsx("Greenhouse_data_row_ASVs.xlsx", sheet = "raw_ASVs", colNames = T, rowNames = T)
-Green_otu_raw <- Green_otu_raw[ ,Green_group$Sample_ID]
-dim(Green_otu_raw)
+ggplot() +
+  geom_point(data_Soil_N, mapping = aes(x = Latitude + offset_mapping[as.character(Year)], 
+                                        y = Soil_N, fill = Year, group = Year),
+             position = position_dodge(width = 0), size = 2.5, pch = 21) +  
+  geom_smooth(data_Soil_N, mapping = aes(x = Latitude, y = Soil_N, color = Year),
+              method = "lm", formula = y ~ x, se = F, size = 0.8, linetype = 1, alpha = 1) + 
+  ggpmisc::stat_poly_eq(data_Soil_N, mapping = aes(x = Latitude, y = Soil_N, color = Year, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")),
+                        formula = y ~ x, parse = TRUE, size = 4) +
+  geom_smooth(data = data_Soil_N, mapping = aes(x = Latitude, y = Soil_N), color = "black", 
+              method = "lm", formula = y ~ x, se = F, linetype = 1, size = 1.5) + 
+  ggpmisc::stat_poly_eq(data = data_Soil_N,  mapping = aes(x = Latitude, y = Soil_N, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")), 
+                        formula = y ~ x, parse = TRUE, size = 4, color = "black", label.y.npc = "center") + 
+  theme_bw() + mytheme + theme(legend.position = c(0.8,0.8)) + 
+  scale_color_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) + 
+  scale_fill_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A"), name = "Year") +
+  scale_y_continuous(expand = expansion(mult = c(0.1, 0.1))) + 
+  labs(x = NULL, y = "Soil total nitrogen content (%, sqrt)", tag = "a") -> p1; p1
 
-par(las = 1, mar = c(5, 5, 4, 2))
-Green_min_depth <- min(rowSums(t(Green_otu_raw)))
-rarecurve(as.data.frame(t(Green_otu_raw)), step = 100, 
-          col = "black", cex = 0.6, label = FALSE, xlab = "", ylab = "", main = "")
-abline(v = Green_min_depth, col = "black", lty = 2, lwd = 2)
-title(main = "Greenhouse experiment", xlab = "Number of sequences", ylab = "Number of ASVs")
 
-################################################################################
-########## Calculation of asymptotic exponential Shannon diversity #############
-################################################################################
+## Soil water content
+data_Wcont <- Field_group[,c("Origin", "Year" ,"Site", "Latitude", "Wcont")]
 
-# Loading the R packages
-library(iNEXT)  # version 3.0.1
+mod5 = lm(Wcont ~ Origin * Year * Latitude, data = data_Wcont)
+shapiro.test(residuals(mod5))
+mod5_emtrends <- emtrends(mod5, pairwise ~ Year, var = "Latitude")
+test(mod5_emtrends, adjust = "BH")
 
-################################# Field survey ################################# 
-# Soil sample grouping information
-Field_group <- read.xlsx("Field_data_group.xlsx", sheet = "field_group", rowNames = T, colNames = T)
-Field_group$Sample_ID <- rownames(Field_group)
+ggplot() +
+  geom_point(data_Wcont, mapping = aes(x = Latitude + offset_mapping[as.character(Year)], 
+                                       y = Wcont, fill = Year, group = Year),
+             position = position_dodge(width = 0), size = 2.5, pch = 21) +  
+  #geom_errorbar(data_Wcont_mean, mapping = aes(x = Latitude, y = mean_Wcont, ymin = mean_Wcont - se_Wcont, ymax = mean_Wcont + se_Wcont, color = Year, group = Year), 
+  #              width = 0.5, size = 0.5, position = position_dodge(width = 0)) +
+  geom_smooth(data_Wcont, mapping = aes(x = Latitude, y = Wcont, color = Year, linetype = Year),
+              method = "lm", formula = y ~ x, se = F, size = 0.8) + 
+  ggpmisc::stat_poly_eq(data_Wcont, mapping = aes(x = Latitude, y = Wcont, color = Year, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")),
+                        formula = y ~ x, parse = TRUE, size = 4) +
+  ## 整体数据集
+  geom_smooth(data = data_Wcont, mapping = aes(x = Latitude, y = Wcont), color = "black", 
+              method = "lm", formula = y ~ x, se = F, linetype = 1, size = 1.5) + 
+  ggpmisc::stat_poly_eq(data = data_Wcont,  mapping = aes(x = Latitude, y = Wcont, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")), 
+                        formula = y ~ x, parse = TRUE, size = 4, color = "black", label.y.npc = "center") + 
+  theme_bw() + mytheme + 
+  scale_color_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) + 
+  scale_fill_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) +
+  scale_linetype_manual(values = c(1,1,2)) + 
+  scale_y_continuous(expand = expansion(mult = c(0.1, 0.1))) + 
+  #scale_x_continuous(expand = expansion(mult = c(0.1, 0.1))) +
+  #scale_x_continuous(breaks=c(23.1,25.2,27.9,30.5,34.6,36.2)) + 
+  labs(x = NULL, y = "Soil water content (g g-1, sqrt)", tag = "b") -> p2; p2
 
-# Soil samples-abundance table
-Field_otu_raw <- read.xlsx("Field_ASVs_raw_data.xlsx", sheet = "raw_ASVs", colNames = T, rowNames = T)
-Field_otu_raw <- Field_otu_raw[ ,Field_group$Sample_ID]
-Field_otu_raw[1:6, 1:6]
-#colSums(Field_otu_raw)
+## Soil pH
+data_Soil_ph <- Field_group[,c("Origin", "Year" ,"Site", "Latitude", "Soil_ph")]
 
-# Calculation of asymptotic exponential Shannon diversity
-results <- list()
-for(i in 1:385) {
-  results[[i]] <- ChaoShannon(as.data.frame(Field_otu_raw)[i], datatype = "abundance")
-  print(paste0(i, ":", colnames(Field_otu_raw)[i]))
-}
+mod3 = lm(Soil_ph ~ Year * Latitude, data = data_Soil_ph)
+anova(mod3)
+mod3_emtrends <- emtrends(mod3, pairwise ~ Year, var = "Latitude")
+test(mod3_emtrends, adjust = "BH")
 
-Field_chao_shannon <- do.call(rbind, results)
-mod_Field <- lm(Estimator ~ Observed, data = Field_chao_shannon)
-summary(mod_Field)
-cor.test(Field_chao_shannon$Observed, Field_chao_shannon$Estimator)
+ggplot() +
+  geom_point(data_Soil_ph, mapping = aes(x = Latitude + offset_mapping[as.character(Year)], 
+                                         y = Soil_ph, fill = Year, group = Year),
+             position = position_dodge(width = 0), size = 2.5, pch = 21) +  
+  geom_smooth(data_Soil_ph, mapping = aes(x = Latitude, y = Soil_ph, color = Year, linetype = Year),
+              method = "lm", formula = y ~ x, se = F, size = 0.8, alpha = 1) + 
+  ggpmisc::stat_poly_eq(data_Soil_ph, mapping = aes(x = Latitude, y = Soil_ph, color = Year, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")),
+                        formula = y ~ x, parse = TRUE, size = 4) +
+  geom_smooth(data = data_Soil_ph, mapping = aes(x = Latitude, y = Soil_ph), color = "black", 
+              method = "lm", formula = y ~ x, se = F, linetype = 1, size = 1.5) + 
+  ggpmisc::stat_poly_eq(data = data_Soil_ph,  mapping = aes(x = Latitude, y = Soil_ph, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")), 
+                        formula = y ~ x, parse = TRUE, size = 4, color = "black", label.y.npc = "center") + 
+  theme_bw() + mytheme + 
+  scale_color_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) + 
+  scale_fill_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) +
+  scale_linetype_manual(values = c(2,1,1)) + 
+  scale_y_continuous(expand = expansion(mult = c(0.1, 0.1))) +
+  labs(x = "Latitude (North degrees)", y = "Soil pH", tag = "c") -> p3; p3
 
-############################# Greenhouse experiment ############################
-# Soil sample grouping information in greenhouse exp.
-Green_group <- read.xlsx("Greenhouse_data_group.xlsx", sheet = "sample_group", colNames = T, rowNames = T)
-Green_group$Sample_ID <- rownames(Green_group)
 
-# Soil samples-abundance table in greenhouse exp.
-Green_otu_raw <- read.xlsx("Greenhouse_ASVs_raw_data.xlsx", sheet = "raw_ASVs", colNames = T, rowNames = T)
-Green_otu_raw <- Green_otu_raw[ ,Green_group$Sample_ID]
-Green_otu_raw[1:6,1:6]
-#colSums(Green_otu_raw)
+## annual average temperature 
+data_Tave <- unique(Field_group[,c("Year" ,"Site", "Latitude", "Tave")])
 
-# Calculation of asymptotic exponential Shannon diversity
-results <- list()
-for(i in 1:162) {
-  results[[i]] <- ChaoShannon(as.data.frame(Green_otu_raw)[i], datatype = "abundance")
-  print(paste0(i, ":", colnames(Green_otu_raw)[i]))
-}
+mod1 = lm(Tave ~ Year * Latitude, data_Tave)
+anova(mod1)
+mod1_emtrends <- emtrends(mod1, pairwise ~ Year, var = "Latitude")
+test(mod1_emtrends, adjust = "BH")
 
-Green_chao_shannon <- do.call(rbind, results)
-mod_Green <- lm(Estimator ~ Observed, data = Green_chao_shannon)
-summary(mod_Green)
-cor.test(Green_chao_shannon$Observed, Green_chao_shannon$Estimator)
+ggplot() +
+  geom_point(data_Tave, mapping = aes(x = Latitude + offset_mapping[as.character(Year)]
+                                      , y = Tave, fill = Year),
+             position = position_dodge(width = 0), size = 2.5, pch = 21) + 
+  geom_smooth(data_Tave, mapping = aes(x = Latitude, y = Tave, color = Year),
+              method = "lm", formula = y ~ x, se = F, size = 0.8, linetype = 1, alpha = 1) + 
+  ggpmisc::stat_poly_eq(data_Tave, mapping = aes(x = Latitude, y = Tave, color = Year, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")),
+                        formula = y ~ x, parse = TRUE, size = 4) + 
+  geom_smooth(data = data_Tave, mapping = aes(x = Latitude, y = Tave), color = "black", 
+              method = "lm", formula = y ~ x, se = F, linetype = 1, size = 1.5) + 
+  ggpmisc::stat_poly_eq(data = data_Tave,  mapping = aes(x = Latitude, y = Tave, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")), 
+                        formula = y ~ x, parse = TRUE, size = 4, color = "black", label.y.npc = "center") + 
+  theme_bw() + mytheme + 
+  theme(legend.position = c(0.85,0.85)) + 
+  scale_color_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) + 
+  scale_fill_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) +
+  scale_y_continuous(expand = expansion(mult = c(0.1, 0.1))) + 
+  labs(x = "Latitude (North degrees)", y = expression("Annual mean temperature ( " * degree * "C)"), tag = "d") -> p4; p4
 
-# Therefore, we used the raw sequencing abundance data for all analysis.
+
+## annual precipitation
+data_Prec <- unique(Field_group[,c("Year" ,"Site", "Latitude", "Prec")])
+# anova(lm(Prec ~ Latitude, subset(data_Prec, Year == "2020")))
+mod2 = lm(Prec ~ Year * Latitude, data_Prec)
+anova(mod2)
+mod2_emtrends <- emtrends(mod2, pairwise ~ Year, var = "Latitude")
+test(mod2_emtrends, adjust = "BH")
+
+ggplot() +
+  geom_point(data_Prec, mapping = aes(x = Latitude + offset_mapping[as.character(Year)], 
+                                      y = Prec, fill = Year),
+             position = position_dodge(width = 0), size = 2.5, pch = 21) + 
+  geom_smooth(data_Prec, mapping = aes(x = Latitude, y = Prec, color = Year, linetype = Year),
+              method = "lm", formula = y ~ x, se = F, size = 0.8, alpha = 1) + 
+  ggpmisc::stat_poly_eq(data_Prec, mapping = aes(x = Latitude, y = Prec, color = Year, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")),
+                        formula = y ~ x, parse = TRUE, size = 4) + 
+  geom_smooth(data = data_Prec, mapping = aes(x = Latitude, y = Prec), color = "black", 
+              method = "lm", formula = y ~ x, se = F, linetype = 1, size = 1.5) + 
+  ggpmisc::stat_poly_eq(data = data_Prec,  mapping = aes(x = Latitude, y = Prec, label = paste(..rr.label.., ..p.value.label.., sep = "~~~")), 
+                        formula = y ~ x, parse = TRUE, size = 4, color = "black", label.y.npc = "center") + 
+  theme_bw() + mytheme +
+  scale_color_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) + 
+  scale_fill_manual(values = c("2018" = "#549299", "2020" = "#FABF5E","2021" = "#9F706A")) +
+  scale_linetype_manual(values = c(1,2,2)) + 
+  #scale_x_continuous(expand = expansion(mult = c(0.1, 0.1))) +
+  scale_y_continuous(expand = expansion(mult = c(0.1, 0.1))) + 
+  #scale_x_continuous(breaks=c(23.1,25.2,27.9,30.5,34.6,36.2)) + 
+  labs(x = "Latitude (North degrees)", y = expression("Annual precipitation (mm)"), tag = "e") -> p5; p5
+
+
+(p1/p4)|(p2/p5)|(p3/p5)
+
 
